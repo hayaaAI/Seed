cda8ef322b94f16f722fbd124ea701d8599d0583
hsiehchingjing <443593695@qq.com>
2017-07-15 16:35:16 +0800

    添加netCore版本客户端

---
+++ Hayaa.Seed/Hayaa.ProgrameSeed_CoreVersion/Model/Config/ComponentService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hayaa.ProgrameSeed.Model.Config
+{
+    [Serializable]
+    public class ComponentService
+    {
+        /// <summary>
+        /// ID
+        /// </summary>	
+        public int ID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 解决方案ID
+        /// </summary>	
+        public Guid SolutionID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序用户ID
+        /// </summary>	
+        public int AppUserID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件ID
+        /// </summary>		
+        public int ComponetID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 服务接口实现类完全限定名
+        /// 形式："类名, 程序集名, Version=1.0.0, Culture=neutral, PublicKeyToken=null"
+        /// </summary>	
+        public string ComponentServiceCompeleteName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 服务接口实现类名
+        ///  形式："类名"
+        /// </summary>	
+        public string ComponentServiceName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序集名称
+        /// </summary>	
+        public string ComponentAssemblyName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序集文件名称
+        /// </summary>	
+        public string ComponentAssemblyFileName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序集文件存储路径
+        /// </summary>	
+        public string ComponentAssemblyFileStorePath
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 接口名称
+        /// </summary>
+        public string ComponentInterface
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序集版本
+        /// </summary>		
+
+        public string AssemblyVersion
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组建类型1是dll2是wcf
+        /// </summary>
+        public int ComponentType { get; set; }
+        /// <summary>
+        /// 服务地址
+        /// </summary>
+        public string ServiceUrl { get; set; }
+    }
+}
---
+++ Hayaa.Seed/Hayaa.ProgrameSeed_CoreVersion/Model/Config/SeedConfig.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hayaa.ProgrameSeed.Model.Config
+{
+    /// <summary>
+    /// 种子服务配置
+    /// </summary>
+    [Serializable]
+  internal  class SeedConfig
+    {
+        /// <summary>
+        /// 种子服务地址
+        /// </summary>
+        public string SeedServerUrl { set; get; }       
+        /// <summary>
+        /// 本地模式下配置目录存储路径
+        /// </summary>
+        public  string LocalConfigDirectoryPath { set; get; }
+        /// <summary>
+        /// 存储路径是否相对路径目录
+        /// </summary>
+        public bool IsVirtualPath { set; get; }
+        /// <summary>
+        /// 配置侦听地址
+        /// </summary>
+        public string AppConfigSentinelUrl { set; get; }
+        /// <summary>
+        /// 传输密匙
+        /// </summary>
+        public string TransfersSecurityKey { get; set; }
+        /// <summary>
+        /// 承载服务程序ID
+        /// </summary>
+        public int AppID { set; get; }
+        /// <summary>
+        /// 程序配置方案ID
+        /// </summary>
+        public Guid AppConfigID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序配置方案版本
+        /// 0表示获取最新
+        /// </summary>
+        public int Version
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 配置模式是否远程化
+        /// </summary>
+        public virtual bool IsRemote
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 程序配置文件名
+        /// </summary>
+        public string AppConfigFileName { get; internal set; }
+    }
+}
