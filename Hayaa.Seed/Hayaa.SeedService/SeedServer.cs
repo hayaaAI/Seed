f7365191017a5ec821991bb06ad04243064600d0
hsiehchingjing <443593695@qq.com>
2017-07-18 21:40:59 +0800

    调整代码结构

--- Hayaa.Seed/Hayaa.SeedService/SeedServer.cs
+++ Hayaa.Seed/Hayaa.SeedService/SeedServer.cs
@@ -8,27 +8,9 @@ using Hayaa.SeedService.DataAccess;
 
 namespace Hayaa.SeedService
 {
-    public partial class SeedServer : IAppService, IComponetService
+    public partial class SeedServer : IAppService
     {
-        public Result DeleteApp(List<int> IDs)
-        {
-            return new Result() { ActionResult = AppDal.Delete(IDs) };
-        }
-
-        public Result DeleteComponent(List<int> IDs)
-        {
-            return new Result() { ActionResult = ComponentDal.Delete(IDs) };
-        }
-
-        public Result DeleteComponentConfig(List<int> IDs)
-        {
-            return new Result() { ActionResult = ComponentConfigDal.Delete(IDs) };
-        }
-
-        public Result DeleteComponentInstance(List<int> IDs)
-        {
-            return new Result() { ActionResult = ComponentInstanceDal.Delete(IDs) };
-        }
+      
 
         public DataResult<int> EditApp(AppInfo info)
         {
@@ -65,29 +47,7 @@ namespace Hayaa.SeedService
             return new Result() { ActionResult = Rel_AppConfig_ComponentConfigDal.EditAppConfigComponents(appConfigID, componentConfigIDs) > 0 };
         }
 
-        public DataResult<int> EditComponent(ComponentInfo info)
-        {
-            int id = 0;
-            if (info.ComponentID == 0) id = ComponentDal.Add(info);
-            else { id = ComponentDal.update(info); if (id > 0) id = info.ComponentID; }
-            return new DataResult<int>() { ActionResult = id > 0, Data = id };
-        }
 
-        public DataResult<int> EditComponentConfig(ComponentConfigInfo info)
-        {
-            int id = 0;
-            if (info.ComponentConfigID == 0) id = ComponentConfigDal.Add(info);
-            else { id = ComponentConfigDal.update(info); if (id > 0) id = info.ComponentConfigID; }
-            return new DataResult<int>() { ActionResult = id > 0, Data = id };
-        }
-
-        public DataResult<int> EditComponentInstance(ComponentInstanceInfo info)
-        {
-            int id = 0;
-            if (info.ComponentInstanceID == 0) id = ComponentInstanceDal.Add(info);
-            else { id = ComponentInstanceDal.update(info); if (id > 0) id = info.ComponentInstanceID; }
-            return new DataResult<int>() { ActionResult = id > 0, Data = id };
-        }
 
         public DataResult<AppInfo> GetApp(int infoID)
         {
@@ -126,40 +86,27 @@ namespace Hayaa.SeedService
             return AppDal.GetGridPager(pageSize, pageIndex, searchName);
         }
 
-        public DataResult<ComponentInfo> GetComponent(int componentID)
-        {
-            var temp = ComponentDal.Get(componentID);
-            return new DataResult<ComponentInfo>() { ActionResult = (temp != null), Data = temp };
-            
-        }
+       
 
-        public DataResult<ComponentConfigInfo> GetComponentConfig(int componentConfigID)
-        {
-            var temp = ComponentConfigDal.Get(componentConfigID);
-            return new DataResult<ComponentConfigInfo>() { ActionResult = (temp != null), Data = temp };
-        }
+     
 
-        public GridPager<ComponentConfigInfo> GetComponentConfigPager(int pageSize, int pageIndex, int componentID, string searchName)
-        {
-            return ComponentConfigDal.GetGridPager(pageSize, pageIndex, searchName);
-        }
+      
+       
 
-        public GridPager<ComponentInfo> GetComponentInfoPager(int pageSize, int pageIndex, string searchName)
-        {
-            return ComponentDal.GetGridPager(pageSize, pageIndex, searchName);
-        }
-
-        public DataResult<ComponentInstanceInfo> GetComponentInstance(int componentInstanceID)
-        {
-            var temp = ComponentInstanceDal.Get(componentInstanceID);
-            return new DataResult<ComponentInstanceInfo>() { ActionResult = (temp != null), Data = temp };
-        }
+       
 
         public GridPager<ComponentInstanceInfo> GetComponentInstancePager(int pageSize, int pageIndex, int componentID, string searchName)
         {
             return ComponentInstanceDal.GetGridPager(pageSize, pageIndex, searchName);
         }
+        public Result DeleteApp(List<int> IDs)
+        {
+            return new Result() { ActionResult = AppDal.Delete(IDs) };
+        }
 
+      
+
+       
         public AppConfig GetRemoteAppConfig(int appID, Guid appConfigID, int version = 0)
         {
             var appConfig = AppConfigDal.Get(appID, appConfigID, version);
--- Hayaa.Seed/Hayaa.SeedService/SeedServer‘1.cs
+++ Hayaa.Seed/Hayaa.SeedService/SeedServer‘1.cs
@@ -8,8 +8,85 @@ using Hayaa.SeedService.DataAccess;
 
 namespace Hayaa.SeedService
 {
-    public partial class SeedServer 
-    {       
+    public partial class SeedServer :  IComponetService
+    {
+      
+
+      
+       
+
+        public DataResult<int> EditComponent(ComponentInfo info)
+        {
+            int id = 0;
+            if (info.ComponentID == 0) id = ComponentDal.Add(info);
+            else { id = ComponentDal.update(info); if (id > 0) id = info.ComponentID; }
+            return new DataResult<int>() { ActionResult = id > 0, Data = id };
+        }
+
+        public DataResult<int> EditComponentConfig(ComponentConfigInfo info)
+        {
+            int id = 0;
+            if (info.ComponentConfigID == 0) id = ComponentConfigDal.Add(info);
+            else { id = ComponentConfigDal.update(info); if (id > 0) id = info.ComponentConfigID; }
+            return new DataResult<int>() { ActionResult = id > 0, Data = id };
+        }
+
+        public DataResult<int> EditComponentInstance(ComponentInstanceInfo info)
+        {
+            int id = 0;
+            if (info.ComponentInstanceID == 0) id = ComponentInstanceDal.Add(info);
+            else { id = ComponentInstanceDal.update(info); if (id > 0) id = info.ComponentInstanceID; }
+            return new DataResult<int>() { ActionResult = id > 0, Data = id };
+        }
+
+       
+       
+
+        public DataResult<ComponentInfo> GetComponent(int componentID)
+        {
+            var temp = ComponentDal.Get(componentID);
+            return new DataResult<ComponentInfo>() { ActionResult = (temp != null), Data = temp };
+            
+        }
+
+        public DataResult<ComponentConfigInfo> GetComponentConfig(int componentConfigID)
+        {
+            var temp = ComponentConfigDal.Get(componentConfigID);
+            return new DataResult<ComponentConfigInfo>() { ActionResult = (temp != null), Data = temp };
+        }
+
+        public GridPager<ComponentConfigInfo> GetComponentConfigPager(int pageSize, int pageIndex, int componentID, string searchName)
+        {
+            return ComponentConfigDal.GetGridPager(pageSize, pageIndex, searchName);
+        }
+
+        public GridPager<ComponentInfo> GetComponentInfoPager(int pageSize, int pageIndex, string searchName)
+        {
+            return ComponentDal.GetGridPager(pageSize, pageIndex, searchName);
+        }
+
+        public DataResult<ComponentInstanceInfo> GetComponentInstance(int componentInstanceID)
+        {
+            var temp = ComponentInstanceDal.Get(componentInstanceID);
+            return new DataResult<ComponentInstanceInfo>() { ActionResult = (temp != null), Data = temp };
+        }
+
+       
+
+        public Result DeleteComponent(List<int> IDs)
+        {
+            return new Result() { ActionResult = ComponentDal.Delete(IDs) };
+        }
+
+        public Result DeleteComponentConfig(List<int> IDs)
+        {
+            return new Result() { ActionResult = ComponentConfigDal.Delete(IDs) };
+        }
+
+        public Result DeleteComponentInstance(List<int> IDs)
+        {
+            return new Result() { ActionResult = ComponentInstanceDal.Delete(IDs) };
+        }
        
     }
 }
